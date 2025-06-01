8387e597a09a386441a9abcddb029464233891cf
Achal <achal.official@gmail.com>
2025-06-01 23:35:32 +0530

    04-Conversion

--- 04-Conversion/main.go
+++ 04-Conversion/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	fmt.Println("Enter the Rating of Pizza :")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error inputing:", err)
+	}
+	fmt.Println("Thanks for rating ", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		fmt.Println("Error in conv:", err)
+	}
+
+	fmt.Println("numRating :", numRating)
+	fmt.Printf("numRating type : %T", numRating)
+
 }
